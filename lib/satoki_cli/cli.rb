712c5712afd37531f8623076f7c645ff48267bbd
satokiy <syamada@netprotections.co.jp>
2023-01-02 14:48:56 +0900

    [add]nippoコマンドを追加

--- lib/satoki_cli/cli.rb
+++ lib/satoki_cli/cli.rb
@@ -1,13 +1,23 @@
 require 'thor'
+require 'dotenv'
 require 'satoki_cli'
 
 module SatokiCli
   class Cli < Thor
+    Dotenv.load
+
     default_command :hello
 
     desc 'hello', 'Output hello. This is default.'
     def hello
       puts 'Hello World!!'
+    end
+
+    desc 'nippo', 'Create daily report page on Scrapbox.'
+    def nippo
+      project = ENV['SCRAPBOX_PROJECT']
+      title = Time.now.strftime('%Y/%-m/%-d')
+      system "open https://scrapbox.io/#{project}/#{title}"
     end
 
   end
